8ef389da8f09a3b5790cd76376f693ca6dfa82f9
youngw77 <youngw2354@gmail.com>
2022-09-30 15:09:20 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -10,7 +10,7 @@ registerBtn.addEventListener("click", register);
 
 function register() {
     if(!id.value) return alert("아이디를 입력해주십시오.");
-    if(password !== checkPassword) {
+    if(password.value !== checkPassword.value) {
         return alert("비밀번호가 일치하지 않습니다");
     }
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -17,9 +17,9 @@ register: (req, res) => {
 };
 
 const process = {
-    login: (req, res) => {
+    login: async(req, res) => {
         const user = new User(req.body);
-        const response = user.login();
+        const response = await user.login();
         return res.json(response);
     }, 
     register: (req, res) => {
